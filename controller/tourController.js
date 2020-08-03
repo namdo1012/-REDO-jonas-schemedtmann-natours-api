5280e789ad99a5d85ede10ec253177dd27c2cfb7
NamDo <namdo10121999@gmail.com>
2020-08-03 17:45:00 +0700

    Add filtering, sorting, paginating and limitField API for query

--- controller/tourController.js
+++ controller/tourController.js
@@ -3,9 +3,57 @@ const Tour = require('../model/tourModel');
 // Get All Tours
 exports.getAllTour = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // console.log(req.query);
+    // const tours = await Tour.find();
+
+    const queryObj = { ...req.query };
+    console.log(queryObj);
+
+    // 1) Filtering ? Why does we need to delete all those fields???
+    // => Answer: To run advanced filtering, all those fields will be excuted later
+    excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    // 2) Advanced Filtering: Operation: >=, <=, >, <
+    // URL: 127.0.0.1:3000/api/v1/tours?duration[gte]=4&difficulty=easy
+    // req.query: { duration: { gte: '4' }, difficulty: 'easy' }
+    // Query in Mongoose: { duration: { $gte: '4' }, difficulty: 'easy' }
+    // ===> the difference is '$'
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // 3) Sorting
+    // URL: ?sort=duration, -difficulty
+    // req.query: { sort: 'duration,-difficulty' }
+    // Query in Mongoose: query.sort(duration -difficulty)
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    // 4) Limit
+    if (req.query.select) {
+      const selectBy = req.query.sort.split(',').join(' ');
+      query = query.select(selectBy);
+    } else {
+      query = query.select('-__v');
+    }
+
+    // 5) Pagination
+    const page = req.query.page * 1 || 1;
+    const limitPerPage = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limitPerPage;
+
+    query = query.skip(skip).limit(limitPerPage);
+
+    const tours = await query;
     res.status(200).json({
       status: 'success',
+      results: tours.length,
       data: {
         tours,
       },
---
+++ utils/APIFeature.js
@@ -0,0 +1,70 @@
+class APIFeature {
+  // query: query to use in mongoose, pass in is Tour.find() with purpose to chain multiple methods (Ex.: Tour.find().split().sort()...)
+  // queryObj: query obj in url
+  constructor(query, queryObj) {
+    this.query = query;
+    this.queryObj = queryObj;
+  }
+
+  filter() {
+    const queryObjTmp = { ...this.queryObj };
+    // console.log(queryObj);
+
+    // 1) Filtering ? Why does we need to delete all those fields???
+    // => Answer: To run advanced filtering, all those fields will be excuted later
+    excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObjTmp[el]);
+
+    // 2) Advanced Filtering: Operation: >=, <=, >, <
+    // URL: 127.0.0.1:3000/api/v1/tours?duration[gte]=4&difficulty=easy
+    // req.query: { duration: { gte: '4' }, difficulty: 'easy' }
+    // Query in Mongoose: { duration: { $gte: '4' }, difficulty: 'easy' }
+    // ===> the difference is '$'
+    let queryStr = JSON.stringify(queryObjTmp);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    this.query = this.query.find(JSON.parse(queryStr));
+
+    return this; // to chain multiple method
+  }
+
+  sort() {
+    // 3) Sorting
+    // URL: ?sort=duration, -difficulty
+    // req.query: { sort: 'duration,-difficulty' }
+    // Query in Mongoose: query.sort(duration -difficulty)
+    if (this.queryObj.sort) {
+      const sortBy = this.queryObj.sort.split(',').join(' ');
+      this.query = this.query.sort(sortBy);
+    } else {
+      this.query = this.query.sort('-createdAt');
+    }
+
+    return this;
+  }
+
+  limitFields() {
+    // 4) Limit
+    if (this.queryObj.select) {
+      const selectBy = this.queryObj.sort.split(',').join(' ');
+      this.query = this.query.select(selectBy);
+    } else {
+      this.query = this.query.select('-__v');
+    }
+
+    return this;
+  }
+
+  paginate() {
+    // 5) Pagination
+    const page = this.queryObj.page * 1 || 1;
+    const limitPerPage = this.queryObj.limit * 1 || 100;
+    const skip = (page - 1) * limitPerPage;
+
+    this.query = this.query.skip(skip).limit(limitPerPage);
+
+    return this;
+  }
+}
+
+module.exports = APIFeature;
