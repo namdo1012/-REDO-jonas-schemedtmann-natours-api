54a767961b2c273359a47e5a8ab5bb8527281db6
NamDo <namdo10121999@gmail.com>
2020-08-14 21:54:04 +0700

    Add Route: Reset Password

--- route/userRoute.js
+++ route/userRoute.js
@@ -5,12 +5,13 @@ const express = require('express');
 
 const router = express.Router();
 
+router.route('/resetPassword/:token').post(authController.resetPassword);
+
 router.route('/signup').post(authController.signup);
 router.route('/login').post(authController.login);
 
 // router.route('/forgotPassword').post(authController.forgotPassword);
 router.post('/forgotPassword', authController.forgotPassword);
-
 router.route('/').get(authController.protect, userController.getAllUsers);
 
 module.exports = router;
