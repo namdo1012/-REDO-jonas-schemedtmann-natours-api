ecc59708e7d0de05b30892ab2bc97a67559106f5
NamDo <namdo10121999@gmail.com>
2020-08-14 22:33:06 +0700

    Add API: Update Me: User update his data by himself

--- controller/userController.js
+++ controller/userController.js
@@ -36,3 +36,28 @@ exports.deleteUser = (req, res) => {
     message: 'This route is not yet defined!',
   });
 };
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // Create error if user update password here
+  if (req.body.password || req.body.passwordConfirm)
+    return next(
+      new AppError(
+        400,
+        'This route is not for password update. Please user /updatePassword instead!'
+      )
+    );
+
+  // Get user from db and update allowed fields [email, name]
+  const user = await User.findById(req.user.id);
+  if (req.body.name) user.name = req.body.name;
+  if (req.body.email) user.email = req.body.email;
+  await user.save();
+
+  // Send response
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user,
+    },
+  });
+});
