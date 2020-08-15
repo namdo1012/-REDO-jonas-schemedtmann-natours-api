8c406968858f4329941076364974cdafe09feb68
NamDo <namdo10121999@gmail.com>
2020-08-15 18:31:20 +0700

    Add API for Review: Get All Reviews

---
+++ controller/reviewController.js
@@ -0,0 +1,12 @@
+const Review = require('./../model/reviewModel');
+const catchAsync = require('./../utils/catchAsync');
+
+exports.getAllReviews = catchAsync(async (req, res, next) => {
+  const reviews = await Review.find();
+  res.status(200).json({
+    status: 'success',
+    data: {
+      reviews,
+    },
+  });
+});
