3aeaf7d6e3cd48522a12a32eba081fe313d9c8a9
NamDo <namdo10121999@gmail.com>
2020-08-15 23:13:25 +0700

    Add API: Get All Reviews Via Tour

--- route/reviewRoute.js
+++ route/reviewRoute.js
@@ -1,10 +1,18 @@
 const mongoose = require('mongoose');
 const express = require('express');
 const reviewControlller = require('./../controller/reviewController');
-const router = express.Router();
+const authController = require('../controller/authController');
+
+const router = express.Router({ mergeParams: true });
 
 router.route('/').get(reviewControlller.getAllReviews);
 router.route('/:id').get(reviewControlller.getReview);
-router.route('/').post(reviewControlller.createReview);
+router
+  .route('/')
+  .post(
+    authController.protect,
+    authController.restrictTo('user'),
+    reviewControlller.createReview
+  );
 
 module.exports = router;
