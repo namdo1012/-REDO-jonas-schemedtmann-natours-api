db3a509b93ce9e893d79fa0b8d16cb2c02b530b1
NamDo <namdo10121999@gmail.com>
2020-08-28 15:45:33 +0700

    !NOT COMPLETED: Tour View: Render Map Section

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,8 +1,17 @@
 // TO MAKE THE MAP APPEAR YOU MUST
 // ADD YOUR ACCESS TOKEN FROM
 // https://account.mapbox.com
+// mapboxgl.accessToken =
+//   'pk.eyJ1IjoibmF0b3Vyc25hbWRvIiwiYSI6ImNrZWRsZ3BlcjB0eTIyc21pbXhtaThmc3AifQ.MMh4VhJtH0mRo9bP9PfhPQ';
+// var map = new mapboxgl.Map({
+//   container: 'map',
+//   style: 'mapbox://styles/mapbox/streets-v11',
+// });
+
+import mapboxgl from './../node_modules/mapbox-gl/dist/mapbox-gl-csp';
+
 mapboxgl.accessToken =
-  'pk.eyJ1IjoibmF0b3Vyc25hbWRvIiwiYSI6ImNrZWNtMWxzcTA4emcyeHM0cjJnbGl6OHYifQ.0OE-7jXO0gLX__3maO4hgw';
+  'pk.eyJ1IjoibmF0b3Vyc25hbWRvIiwiYSI6ImNrZWRsZ3BlcjB0eTIyc21pbXhtaThmc3AifQ.MMh4VhJtH0mRo9bP9PfhPQ';
 var map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/streets-v11',
