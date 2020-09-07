fe051ce460364a5a06b83fec6e27eb42074acf66
NamDo <namdo10121999@gmail.com>
2020-09-07 22:16:35 +0700

    Toggle updatePassword Submit

--- public/js/index.js
+++ public/js/index.js
@@ -2,10 +2,12 @@ import '@babel/polyfill';
 import { login } from './login';
 import { logout } from './logout';
 import { updateSetting } from './updateSetting';
+import { updatePassword } from './updatePassword';
 
 const loginForm = document.querySelector('.form--login');
 const logoutButton = document.querySelector('.nav__el--logout');
 const userDataForm = document.querySelector('.form-user-data');
+const userPasswordForm = document.querySelector('.form-user-password');
 
 if (loginForm)
   loginForm.addEventListener('submit', (e) => {
@@ -23,11 +25,26 @@ if (logoutButton) {
 }
 
 if (userDataForm) {
+  console.log('There is userdataform');
   userDataForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
 
     updateSetting(name, email);
+  });
+}
+
+if (userPasswordForm) {
+  console.log('There is userpasswordform');
+  userPasswordForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const currentPassword = document.getElementById('password-current').value;
+    const newPassword = document.getElementById('password').value;
+    const newPasswordConfirm = document.getElementById('password-confirm')
+      .value;
+
+    updatePassword(currentPassword, newPassword, newPasswordConfirm);
+    // console.log({ currentPassword, newPassword, newPasswordConfirm });
   });
 }
