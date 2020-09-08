12834a801fccb8145544881bc49173a0f248c576
NamDo <namdo10121999@gmail.com>
2020-09-08 17:54:38 +0700

    Upload user's avatar with multer diskStorage

--- controller/userController.js
+++ controller/userController.js
@@ -1,7 +1,23 @@
 const User = require('./../model/userModel');
 const catchAsync = require('./../utils/catchAsync');
-
+const multer = require('multer');
 const handlerFactory = require('./handlerFactory');
+
+// Config multer
+const multerStorage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/img/users/');
+  },
+  filename: function (req, file, cb) {
+    const ext = file.mimetype.split('/')[1];
+    const filename = `user-${req.user.id}-${Date.now()}.${ext}`;
+    cb(null, filename);
+  },
+});
+
+const upload = multer({ storage: multerStorage });
+
+exports.uploadPhoto = upload.single('photo');
 
 exports.getMe = (req, res, next) => {
   req.params.id = req.user.id;
@@ -24,6 +40,9 @@ exports.updateUser = handlerFactory.updateModel(User);
 exports.deleteUser = handlerFactory.deleteModel(User);
 
 exports.updateMe = catchAsync(async (req, res, next) => {
+  console.log(req.file);
+  console.log(req.body);
+
   // Create error if user update password here
   if (req.body.password || req.body.passwordConfirm)
     return next(
@@ -38,6 +57,7 @@ exports.updateMe = catchAsync(async (req, res, next) => {
   // console.log(user);
   if (req.body.name) user.name = req.body.name;
   if (req.body.email) user.email = req.body.email;
+  if (req.file) user.photo = req.file.filename;
   await user.save();
 
   // Send response
